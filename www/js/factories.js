80b71b14d43709ab0759d818afed511eb23fb4a4
millionhari <mikemhari@gmail.com>
2015-03-25 11:39:04 -0700

    Refactor controller functions into factories

--- www/js/factories.js
+++ www/js/factories.js
@@ -1,5 +1,19 @@
 angular.module('offTheTruck.factories', [])
 .factory('User', function(){
+    //Gain access to Firebase
+  var ref = new Firebase("https://offthetruck.firebaseio.com/");
+  var truckRef = new Firebase("https://offthetruck.firebaseio.com/Trucks");
+
+  var addTruck = function(user){
+    truckRef.child(user.uid).set({
+      truckname: user.truckname,
+        email: user.email,
+        isServing: false,
+        lat: null,
+        long: null
+    });
+  };
+
   return {
     uid: null
   };
--- www/js/userCtrl.js
+++ www/js/userCtrl.js
@@ -12,13 +12,7 @@ function($scope, User, Truck, $state){
     /*Add truck adds a truck to our Firebase collection.
     We initialize lat/long to null but it is updated when they are 'ready' to serve.*/
     $scope.addTruck = function(user){
-      truckRef.child(user.uid).set({
-        truckname: user.truckname,
-        email: user.email,
-        isServing: false,
-        lat: null,
-        long: null
-      });
+      addTruck(user);
     };
 
     //This is boilerplate log-in code made available to us from Firebase.
