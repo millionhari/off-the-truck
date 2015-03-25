b56a3500e91de599d6833bb94c0ef44255002101
millionhari <mikemhari@gmail.com>
2015-03-25 14:59:43 -0700

    (feat) Add new User Factory file

---
+++ www/js/userFactory.js
@@ -0,0 +1,21 @@
+angular.module('offTheTruck.factories', [])
+.factory('User', function(){
+    //Gain access to Firebase
+  var ref = new Firebase("https://offthetruck.firebaseio.com/");
+  var truckRef = new Firebase("https://offthetruck.firebaseio.com/Trucks");
+
+  var addTruckToFirebase = function(user){
+    truckRef.child(user.uid).set({
+      truckname: user.truckname,
+        email: user.email,
+        isServing: false,
+        lat: null,
+        long: null
+    });
+  };
+
+  return {
+    addTruck : addTruck,
+    uid: null
+  };
+})
