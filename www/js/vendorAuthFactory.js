44f9a6dd100c371d4aa774cb513962145c0eb595
annekef <anneke.floor@gmail.com>
2015-03-27 15:41:34 -0700

    (feat) Work toward display vendor info after db call

--- www/js/vendorAuthFactory.js
+++ www/js/vendorAuthFactory.js
@@ -18,76 +18,76 @@ angular.module('offTheTruck.vendorAuthFactory', [])
   var notUniqueEmail;
   var notUser;
 
-  return {
+  // Use the Firebase createUser method to create a new Vendor user in the data store
+  // Assign the uid returned by the method to the vendorAuthRef object
+  var signupVendor = function(vendor) {
+    /*createUser is a method made available to us by Firebase, automatically creating users.
+    The only data it can take is email and password.*/
+    ref.createUser({
+      email: vendor.email,
+      password: vendor.password
+    },function(error, vendorData) {
+        if(error){
+          console.log("Error creating vendor:", error);
+          notUniqueEmail = true;
+        } else {
+          console.log("Successfully created vendor account with uid:", vendorData.uid);
+          //We add the user to our userID object for use later in our app
+          vendorAuthRef.uid = vendorData.uid;
 
+          //Once an account is created, we then add a truck to our Trucks database
+          truckRef.child(vendorData.uid).set({
+            truckName: vendor.truckName,
+            email: vendor.email,
+            isServing: false,
+            lat: 0,
+            long: 0
+          });
+
+          $state.go('login');
+        }
+      }
+    ); 
+  };
+
+  // Use the Firebase loginVendor method to attempt to login Vendor user
+  // Assign the uid returned by the method to the vendorAuthRef object
+  var loginVendor = function(vendor) {
+    ref.authWithPassword({
+      email: vendor.email,
+      password: vendor.password
+    },function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+          notUser = true;
+        } else {
+
+          //We save user ID to this variable, which is defined and controlled in the factory
+          vendorAuthRef.uid = authData.uid;
+          
+          console.log('Vendor logged in successfully. uid: ', vendorAuthRef.uid);
+
+          /*This code users user ID to reference our Firebase Trucks collection to find the name of the Truck associated with it.
+          Snapshot is the JSON object that contains just the truck name*/
+          truckRef.child(authData.uid).child('truckName').on('value', function(snapshot){
+            vendorAuthRef.truckName = snapshot.val();
+
+            //Then redirect user to the vendor page.
+            $state.go('vendor.home');
+          });
+        }
+      }
+    );
+  };
+
+  return {
     ref: ref,
     truckRef: truckRef,
     vendorAuthRef: vendorAuthRef,
-
     notUniqueEmail: notUniqueEmail,
     notUser: notUser,
 
-    // Use the Firebase createUser method to create a new Vendor user in the data store
-    // Assign the uid returned by the method to the vendorAuthRef object
-    signupVendor: function(vendor) {
-
-      /*createUser is a method made available to us by Firebase, automatically creating users.
-      The only data it can take is email and password.*/
-      ref.createUser({
-        email: vendor.email,
-        password: vendor.password
-      },function(error, vendorData) {
-          if(error){
-            console.log("Error creating vendor:", error);
-            notUniqueEmail = true;
-          } else {
-            console.log("Successfully created vendor account with uid:", vendorData.uid);
-            //We add the user to our userID object for use later in our app
-            vendorAuthRef.uid = vendorData.uid;
-
-            //Once an account is created, we then add a truck to our Trucks database
-            truckRef.child(vendorData.uid).set({
-              truckName: vendor.truckName,
-              email: vendor.email,
-              isServing: false,
-              lat: 0,
-              long: 0
-            });
-
-            $state.go('login');
-          }
-        }
-      ); 
-    },
-
-    // Use the Firebase loginVendor method to attempt to login Vendor user
-    // Assign the uid returned by the method to the vendorAuthRef object
-    loginVendor: function(vendor) {
-      ref.authWithPassword({
-        email: vendor.email,
-        password: vendor.password
-      },function(error, authData) {
-          if (error) {
-            console.log("Login Failed!", error);
-            notUser = true;
-          } else {
-
-            //We save user ID to this variable, which is defined and controlled in the factory
-            vendorAuthRef.uid = authData.uid;
-            
-            console.log('Vendor logged in successfully. uid: ', vendorAuthRef.uid);
-            /*This code users user ID to reference our Firebase Trucks collection to find the name of the Truck associated with it.
-            Snapshot is the JSON object that contains just the truck name*/
-            truckRef.child(authData.uid).child('truckName').on('value', function(snapshot){
-              vendorAuthRef.truckName = snapshot.val();
-              //Then redirect user to the vendor page.
-              $state.go('vendor');
-            });
-          }
-        }
-      );
-    }
-    
+    signupVendor: signupVendor,
+    loginVendor: loginVendor,
   };
-
 }]);
--- www/js/vendorCtrl.js
+++ www/js/vendorCtrl.js
@@ -5,22 +5,30 @@ angular.module('offTheTruck.vendorCtrl', [])
   $scope.statusText = 'Not Serving';
   $scope.notEditable = true;
 
+  // set up a watch to listen for when uid in factory changes
+  // once uid is changed (from null to an actual logged-in user),
+  // get the vendor's information from Firebase.
+  //
+  // The first function passed to $watch is our listener. The second
+  // is what we want to do on change.
+  // $scope.$watch(function($scope){
+  //   return $scope.uid;
+  // },function(newUid){
+  //   console.log('newUid', newUid);
+  //   $scope.vendorRef = new Firebase("https://offthetruck.firebaseio.com/Trucks/" + newUid);
+  //   $scope.vendor = $firebaseObject($scope.vendorRef);        
+  // });
+
   $scope.vendorRef = new Firebase("https://offthetruck.firebaseio.com/Trucks/simplelogin:57");
-  $scope.vendor = $firebaseObject($scope.vendorRef);
+  $scope.vendor = $firebaseObject($scope.vendorRef);        
 
   $scope.vendor.$loaded().then(function(){
     $scope.setStatusText();
-
-    console.log('VENDOR AUTH', VendorAuth.vendorAuthRef);
-    console.log('vendor?', $scope.vendor);
-    console.log('truckName', $scope.vendor.truckName);
+    // console.log('VENDOR AUTH', VendorAuth.vendorAuthRef);
   });
 
 
-
-
   // get the vendor's information to populate views with
-
   $scope.getTruckInfo = function(currentTruck){
     console.log('truck', truck);
   };
@@ -38,8 +46,6 @@ angular.module('offTheTruck.vendorCtrl', [])
   };
 
   $scope.setStatusText = function(){
-
-    console.log('vendor', $scope.vendor);
     // check isServing
     // set status text accordingly
     if($scope.vendor.isServing){
